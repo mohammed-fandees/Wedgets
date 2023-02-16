0c391b2e68558d62c158a34f2000aa9ad2cfc5b1
Mohammed Fandees <mohammed.fandees@gmail.com>
2023-02-16 04:30:36 +0200

    add 3 menus

---
+++ js/script.js
@@ -0,0 +1,39 @@
+class Navbar {
+    constructor(notficationMenu, appsMenu, userMenu) {
+        this.notficationMenu = notficationMenu;
+        this.appsMenu = appsMenu;
+        this.userMenu = userMenu;
+    }
+
+    notifcationEvent() {
+        this.notficationMenu.classList.toggle('menu-visible');
+        this.userMenu.classList.remove('menu-visible');
+        this.appsMenu.classList.remove('menu-visible');
+    }
+
+    appsEvent() {
+        this.appsMenu.classList.toggle('menu-visible');
+        this.userMenu.classList.remove('menu-visible');
+        this.notficationMenu.classList.remove('menu-visible');
+    }
+
+    userEvent() {
+        this.userMenu.classList.toggle('menu-visible');
+        this.notficationMenu.classList.remove('menu-visible');
+        this.appsMenu.classList.remove('menu-visible');
+    }
+}
+
+let navbar = new Navbar(
+    document.querySelector('#notfications-menu'),
+    document.querySelector('#apps-menu'),
+    document.querySelector('#user-menu')
+);
+
+document.querySelector('#notfications').addEventListener('click', _ => navbar.notifcationEvent());
+document.querySelector('.choose').addEventListener('click', _ => navbar.appsEvent());
+
+document.querySelector('#userPhoto').addEventListener('click', function() {
+    this.classList.toggle('click-photo');
+    navbar.userEvent();
+});
